e1a24f3221acf27434bc1c6a068558074b52da8c
MichalBednarczyk1993 <MichalBednarczyk1993@gmail.com>
2021-08-04 20:40:56 +0200

    Create view and scene, add firts item

--- EasyRider_Qt/main.cpp
+++ EasyRider_Qt/main.cpp
@@ -1,11 +1,20 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QGraphicsRectItem *item = new QGraphicsRectItem();
+    item->setRect(0, 0, 30, 30);
+
+    QGraphicsScene *scene = new QGraphicsScene();
+    scene->addItem(item);
+
+    QGraphicsView *view = new QGraphicsView(scene);
+    view->show();
+
     return a.exec();
 }
